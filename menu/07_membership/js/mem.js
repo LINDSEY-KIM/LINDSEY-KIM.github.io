33b959f71e44de62ec852cc78e5fc5f3ffa70fe7
LINDSEY-KIM <88068327+LINDSEY-KIM@users.noreply.github.com>
2022-06-28 17:33:47 +0900

    Delete menu directory

--- menu/07_membership/js/mem.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  const hamBtnEl = document.querySelector(".ham_btn");
-  const tabMenuEl = document.querySelector(".tab_menu_wrap");
-
-  hamBtnEl.addEventListener("click", () => {
-    hamBtnEl.classList.toggle("active");
-    tabMenuEl.classList.toggle("slide");
-  });
-})();
